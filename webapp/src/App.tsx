e5044749a2822f4b9fc4d935c9b3808e522c4669
Tati Trif <tatitrif@mail.ru>
2025-01-10 20:26:30 +0300

    add conf prettier

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,31 +1,31 @@
 export const App = () => {
   const ideas = [
     {
-      nick: "cool-idea-nick-1",
-      name: "Idea 1",
-      description: "Description of idea 1...",
+      nick: 'cool-idea-nick-1',
+      name: 'Idea 1',
+      description: 'Description of idea 1...',
     },
     {
-      nick: "cool-idea-nick-2",
-      name: "Idea 2",
-      description: "Description of idea 2...",
+      nick: 'cool-idea-nick-2',
+      name: 'Idea 2',
+      description: 'Description of idea 2...',
     },
     {
-      nick: "cool-idea-nick-3",
-      name: "Idea 3",
-      description: "Description of idea 3...",
+      nick: 'cool-idea-nick-3',
+      name: 'Idea 3',
+      description: 'Description of idea 3...',
     },
     {
-      nick: "cool-idea-nick-4",
-      name: "Idea 4",
-      description: "Description of idea 4...",
+      nick: 'cool-idea-nick-4',
+      name: 'Idea 4',
+      description: 'Description of idea 4...',
     },
     {
-      nick: "cool-idea-nick-5",
-      name: "Idea 5",
-      description: "Description of idea 5...",
+      nick: 'cool-idea-nick-5',
+      name: 'Idea 5',
+      description: 'Description of idea 5...',
     },
-  ];
+  ]
   return (
     <div>
       <h1>Ideanick</h1>
@@ -36,5 +36,5 @@ export const App = () => {
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
