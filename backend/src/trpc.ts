895d82d40ae61096763e24e5553bee399727770b
Tati Trif <tatitrif@mail.ru>
2025-01-12 04:45:43 +0300

    update fake content with zod

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server'
 import _ from 'lodash'
+import { z } from 'zod'
 
 const ideas = _.times(100, (i) => ({
   nick: `cool-idea-nick-${i}`,
@@ -8,11 +9,20 @@ const ideas = _.times(100, (i) => ({
   text: _.times(100, (j) => `<p>Text paragrph ${j} of idea ${i}...</p>`).join(''),
 }))
 
-
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) }
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick)
+      return { idea: idea || null }
+    }),
 })
