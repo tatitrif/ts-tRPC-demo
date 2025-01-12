b0f328af0073b47f0bb0b5a572b3e588163e1d27
Tati Trif <tatitrif@mail.ru>
2025-01-12 15:50:14 +0300

    update trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,10 +11,6 @@ expressApp.get('/ping', (req, res) => {
 })
 applyTrpcToExpressApp(expressApp, trpcRouter)
 
-
-
-
-
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000')
 })
