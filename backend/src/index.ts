175750211979a7a48c18778ef340c8f291785e10
Tati Trif <tatitrif@mail.ru>
2025-01-12 15:47:56 +0300

    update trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,19 @@
-import * as trpcExpress from '@trpc/server/adapters/express'
 import cors from 'cors'
 import express from 'express'
 
-import { trpcRouter } from './trpc'
+import { applyTrpcToExpressApp } from './lib/trpc'
+import { trpcRouter } from './router'
 
 const expressApp = express()
 expressApp.use(cors())
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
 })
+applyTrpcToExpressApp(expressApp, trpcRouter)
 
-expressApp.use(
-  '/trpc',
-  trpcExpress.createExpressMiddleware({
-    router: trpcRouter,
-  })
-)
+
+
+
 
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000')
