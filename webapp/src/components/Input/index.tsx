9551f6d44daffeb40e6cbbd22aacc1b3a8815f36
Tati Trif <tatitrif@mail.ru>
2025-01-12 13:19:45 +0300

    update formik

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -3,6 +3,8 @@ import { type FormikProps } from 'formik'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Input = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const value = formik.values[name]
+  const error = formik.errors[name] as string | undefined
+  const touched = formik.touched[name]
 
   return (
     <div style={{ marginBottom: 10 }}>
@@ -13,10 +15,14 @@ export const Input = ({ name, label, formik }: { name: string; label: string; fo
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
+        onBlur={() => {
+          void formik.setFieldTouched(name)
+        }}
         value={value}
         name={name}
         id={name}
       />
+      {!!touched && !!error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   )
 }
