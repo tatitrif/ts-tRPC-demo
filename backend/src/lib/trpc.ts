407605209bbea9be4beca89d37849188af5ab7cb
Tati Trif <tatitrif@mail.ru>
2025-01-13 19:05:19 +0300

    add trpc-playground

--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -1,18 +1,30 @@
 import { initTRPC } from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { type Express } from 'express'
+import { expressHandler } from 'trpc-playground/handlers/express'
 
 import { type TrpcRouter } from '../router'
 import { type AppContext } from './ctx'
 
 export const trpc = initTRPC.context<AppContext>().create()
 
-export const applyTrpcToExpressApp = (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
+export const applyTrpcToExpressApp = async (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
   expressApp.use(
     '/trpc',
     trpcExpress.createExpressMiddleware({
       router: trpcRouter,
       createContext: () => appContext,
+    })
+  )
+  expressApp.use(
+    '/trpc-playground',
+    await expressHandler({
+      trpcApiEndpoint: '/trpc',
+      playgroundEndpoint: '/trpc-playground',
+      router: trpcRouter,
+      request: {
+        superjson: true,
+      },
     })
   )
 }
