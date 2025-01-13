ba8d41aa802d84bc195270150c93af2d69b538bd
Tati Trif <tatitrif@mail.ru>
2025-01-13 21:27:15 +0300

    add relation

--- backend/src/router/getIdea/index.ts
+++ backend/src/router/getIdea/index.ts
@@ -13,6 +13,14 @@ export const getIdeaTrpcRoute = trpc.procedure
       where: {
         nick: input.ideaNick,
       },
+      include: {
+        author: {
+          select: {
+            id: true,
+            nick: true,
+          },
+        },
+      },
     })
 
     return { idea }
