43b71653a82ce88722db5af181bb7039a807ae06
Tati Trif <tatitrif@mail.ru>
2025-01-13 22:08:14 +0300

    update form

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,7 +1,4 @@
 import { zCreateIdeaTrpcInput } from '@ideanick/backend/src/router/createIdea/input'
-import { useFormik } from 'formik'
-import { withZodSchema } from 'formik-validator-zod'
-import { useState } from 'react'
 
 import { Alert } from '../../components/Alert'
 import { Button } from '../../components/Button'
@@ -9,36 +6,25 @@ import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
+import { useForm } from '../../lib/form'
 import { trpc } from '../../lib/trpc'
 
 export const NewIdeaPage = () => {
-  const [successMessageVisible, setSuccessMessageVisible] = useState(false)
-  const [submittingError, setSubmittingError] = useState<string | null>(null)
   const createIdea = trpc.createIdea.useMutation()
-  const formik = useFormik({
+  const { formik, buttonProps, alertProps } = useForm({
     initialValues: {
       name: '',
       nick: '',
       description: '',
       text: '',
     },
-    validate: withZodSchema(zCreateIdeaTrpcInput),
+    validationSchema: zCreateIdeaTrpcInput,
     onSubmit: async (values) => {
-      try {
-        await createIdea.mutateAsync(values)
-        formik.resetForm()
-        setSuccessMessageVisible(true)
-        setTimeout(() => {
-          setSuccessMessageVisible(false)
-        }, 3000)
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        setSubmittingError(error.message)
-        setTimeout(() => {
-          setSubmittingError(null)
-        }, 3000)
-      }
+      await createIdea.mutateAsync(values)
+      formik.resetForm()
     },
+    successMessage: 'Idea created!',
+    showValidationAlert: true,
   })
 
   return (
@@ -54,10 +40,8 @@ export const NewIdeaPage = () => {
           <Input name="nick" label="Nick" formik={formik} />
           <Input name="description" label="Description" formik={formik} maxWidth={500} />
           <Textarea name="text" label="Text" formik={formik} />
-          {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
-          {!!submittingError && <Alert color="red">{submittingError}</Alert>}
-          {successMessageVisible && <Alert color="green">Idea created!</Alert>}
-          <Button loading={formik.isSubmitting}>Create Idea</Button>
+          <Alert {...alertProps} />
+          <Button {...buttonProps}>Create Idea</Button>
         </FormItems>
       </form>
     </Segment>
--- webapp/src/pages/SignInPage/index.tsx
+++ webapp/src/pages/SignInPage/index.tsx
@@ -1,8 +1,5 @@
 import { zSignInTrpcInput } from '@ideanick/backend/src/router/signIn/input'
-import { useFormik } from 'formik'
-import { withZodSchema } from 'formik-validator-zod'
 import Cookies from 'js-cookie'
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { Alert } from '../../components/Alert'
@@ -10,32 +7,28 @@ import { Button } from '../../components/Button'
 import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
+import { useForm } from '../../lib/form'
 import { getAllIdeasRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 
 export const SignInPage = () => {
   const navigate = useNavigate()
   const trpcUtils = trpc.useContext()
-  const [submittingError, setSubmittingError] = useState<string | null>(null)
   const signIn = trpc.signIn.useMutation()
-  const formik = useFormik({
+
+  const { formik, buttonProps, alertProps } = useForm({
     initialValues: {
       nick: '',
       password: '',
     },
-    validate: withZodSchema(zSignInTrpcInput),
+    validationSchema: zSignInTrpcInput,
     onSubmit: async (values) => {
-      try {
-        setSubmittingError(null)
-        const { token } = await signIn.mutateAsync(values)
-        Cookies.set('token', token, { expires: 99999 })
-        void trpcUtils.invalidate()
-        navigate(getAllIdeasRoute())
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (err: any) {
-        setSubmittingError(err.message)
-      }
+      const { token } = await signIn.mutateAsync(values)
+      Cookies.set('token', token, { expires: 99999 })
+      void trpcUtils.invalidate()
+      navigate(getAllIdeasRoute())
     },
+    resetOnSuccess: false,
   })
 
   return (
@@ -44,9 +37,8 @@ export const SignInPage = () => {
         <FormItems>
           <Input label="Nick" name="nick" formik={formik} />
           <Input label="Password" name="password" type="password" formik={formik} />
-          {!formik.isValid && !!formik.submitCount && <Alert color="red">Some fields are invalid</Alert>}
-          {submittingError && <Alert color="red">{submittingError}</Alert>}
-          <Button loading={formik.isSubmitting}>Sign In</Button>
+          <Alert {...alertProps} />
+          <Button {...buttonProps}>Sign In</Button>
         </FormItems>
       </form>
     </Segment>
