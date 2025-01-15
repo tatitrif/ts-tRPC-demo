40f7cb41749f3693906b54986b5e79eb75e5194f
Tati Trif <tatitrif@mail.ru>
2025-01-15 19:57:04 +0300

    add loging front

--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -1,6 +1,6 @@
 import type { TrpcRouter } from '@ideanick/backend/src/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
+import { httpBatchLink, loggerLink } from '@trpc/client'
 import { createTRPCReact } from '@trpc/react-query'
 import Cookies from 'js-cookie'
 
@@ -19,6 +19,9 @@ const queryClient = new QueryClient({
 
 const trpcClient = trpc.createClient({
   links: [
+    loggerLink({
+      enabled: () => env.NODE_ENV === 'development',
+    }),
     httpBatchLink({
       url: env.VITE_BACKEND_TRPC_URL,
       headers: () => {
